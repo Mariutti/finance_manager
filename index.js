8ae0751824829b0b01f22e12ef70070c28fe69c3
Pedro Alencar <pedromariutti@gmail.com>
2023-02-23 22:01:12 -0300

    Commit exercise

--- index.js
+++ index.js
@@ -1,37 +1,57 @@
 const valueInput = document.getElementById('value-input');
 const descriptionInput = document.getElementById('description-input');
 
+class FinancialTransaction {
+  description;
+  value;
+  constructor(description, value) {
+    this.description = description;
+    this.value = value;
+  }
+}
+
+class FinancialTransactionManager {
+  earningsList;
+  expensesList;
+
+  constructor(earningsArray = [], expensesArray = []) {
+    this.earningsList = earningsArray;
+    this.expensesList = expensesArray;
+
+    this.createScreen();
+  }
+
+  createScreen() {
+    this.gerenateLists(this.expensesList, 'expenses-list');
+    this.gerenateLists(this.earningsList, 'earnings-list');
+  }
+
+  gerenateLists(array, listId) {
+    for (let item of array) {
+      var li = document.createElement('li');
+      li.innerText = `${item.description}: R$ ${item.value}`;
+      document.getElementById(`${listId}`).appendChild(li);
+    }
+  }
+}
+
+const financialTransactionManager = new FinancialTransactionManager(
+  [
+    new FinancialTransaction('Salário', 4000),
+    new FinancialTransaction('Freela', 400),
+  ],
+  [
+    new FinancialTransaction('Alimentação', 650),
+    new FinancialTransaction('PSN', 40),
+    new FinancialTransaction('Energia', 500),
+  ]
+);
+
 var mesAtual = 'janeiro';
 var entradas = 8000;
 
 var saidas = 400;
 var saldo = 1000;
-
-var earningsList = [
-  {
-    description: 'Salário',
-    value: 4000,
-  },
-  {
-    description: 'Freela',
-    value: 400,
-  },
-];
-
-var expensesList = [
-  {
-    description: 'Alimentação',
-    value: 650,
-  },
-  {
-    description: 'PSN',
-    value: 40,
-  },
-  {
-    description: 'Energia',
-    value: 500,
-  },
-];
 
 // Mês
 document.getElementById('month').innerHTML = mesAtual;
@@ -81,15 +101,3 @@ function addEarning() {
 
   document.getElementById('earnings-list').appendChild(li);
 }
-
-function createLists(array, listId) {
-  for (let item of array) {
-    var li = document.createElement('li');
-    li.innerText = `${item.description}: R$ ${item.value}`;
-    document.getElementById(`${listId}`).appendChild(li);
-  }
-}
-
-createLists(earningsList, 'earnings-list');
-
-createLists(expensesList, 'expenses-list');
